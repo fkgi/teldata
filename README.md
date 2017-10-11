8281762d26c054ec9b33af6d11806a6da53c84a4
fkgi <ats.fukagai@gmail.com>
2017-10-11 21:26:45 +0900

    modify
    Signed-off-by: fkgi <ats.fukagai@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # teldata
 General data format for telecommunication.
+# License
+BSD 3-Clause
---
+++ error.go
@@ -0,0 +1,13 @@
+package teldata
+
+import "fmt"
+
+// InvalidDataError show invalid data
+type InvalidDataError struct {
+	Name  string
+	Bytes []byte
+}
+
+func (e *InvalidDataError) Error() string {
+	return fmt.Sprintf("invalid data for %s: % x", e.Name, e.Bytes)
+}
