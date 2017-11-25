41b097e0f2414b681719220e60d9286b09761860
fkgi <ats.fukagai@gmail.com>
2017-11-25 22:41:29 +0900

    modify
    Signed-off-by: fkgi <ats.fukagai@gmail.com>

--- e164.go
+++ e164.go
@@ -1,41 +1,36 @@
 package teldata
 
+import "encoding/json"
+
 // E164 value
-type E164 struct {
-	tbcd TBCD
-}
+type E164 TBCD
 
 // ParseE164 makes E164 data from string
 func ParseE164(s string) (m E164, e error) {
-	m.tbcd, e = ParseTBCD(s)
-	if e == nil && m.tbcd.Length() > 15 {
+	t, e := ParseTBCD(s)
+	if e == nil && t.Length() > 15 {
 		e = InvalidDataError{Name: "E164", Bytes: []byte(s)}
 	}
+	m = E164(t)
 	return
 }
 
 func (m E164) String() string {
-	if m.tbcd == nil {
-		return ""
-	}
-	return m.tbcd.String()
+	return TBCD(m).String()
 }
 
 // Bytes returns TBCD format byte data
 func (m E164) Bytes() []byte {
-	if m.tbcd == nil {
-		return []byte{}
-	}
-	r := make([]byte, len(m.tbcd))
-	copy(r, m.tbcd)
+	r := make([]byte, len(m))
+	copy(r, m)
 	return r
 }
 
 // ToE164 makes E164 from this TBCD value
 func ToE164(b []byte) (m E164, e error) {
-	m.tbcd = make([]byte, len(b))
-	copy(m.tbcd, b)
-	if m.tbcd.Length() > 15 {
+	m = make([]byte, len(b))
+	copy(m, b)
+	if m.Length() > 15 {
 		e = InvalidDataError{Name: "E164", Bytes: b}
 	}
 	return
@@ -43,8 +38,22 @@ func ToE164(b []byte) (m E164, e error) {
 
 // Length of this E164
 func (m E164) Length() int {
-	if m.tbcd == nil {
-		return 0
+	return TBCD(m).Length()
+}
+
+// MarshalJSON provide custom marshaller
+func (m E164) MarshalJSON() ([]byte, error) {
+	return json.Marshal(m.String())
+}
+
+// UnmarshalJSON provide custom marshaller
+func (m *E164) UnmarshalJSON(b []byte) (e error) {
+	var s string
+	if e = json.Unmarshal(b, &s); e != nil {
+		return e
 	}
-	return m.tbcd.Length()
+	if len(s) != 0 {
+		*m, e = ParseE164(s)
+	}
+	return e
 }
--- imsi.go
+++ imsi.go
@@ -1,52 +1,69 @@
 package teldata
 
+import "encoding/json"
+
 // IMSI value
-type IMSI struct {
-	string
-}
+type IMSI string
 
 // ParseIMSI makes IMSI data from string
 func ParseIMSI(s string) (IMSI, error) {
 	for _, r := range s {
 		if r < '0' || r > '9' {
-			return IMSI{}, InvalidDataError{Name: "IMSI", Bytes: []byte(s)}
+			return IMSI(""), InvalidDataError{Name: "IMSI", Bytes: []byte(s)}
 		}
 	}
 	if len(s) < 5 || len(s) > 15 {
-		return IMSI{}, InvalidDataError{Name: "IMSI", Bytes: []byte(s)}
+		return IMSI(""), InvalidDataError{Name: "IMSI", Bytes: []byte(s)}
 	}
-	return IMSI{s}, nil
+	return IMSI(s), nil
 }
 
 func (i IMSI) String() string {
-	return i.string
+	return string(i)
 }
 
 // Length of this IMSI
 func (i IMSI) Length() int {
-	return len(i.string)
+	return len(i)
+}
+
+// MarshalJSON provide custom marshaller
+func (i IMSI) MarshalJSON() ([]byte, error) {
+	return json.Marshal(string(i))
+}
+
+// UnmarshalJSON provide custom marshaller
+func (i *IMSI) UnmarshalJSON(b []byte) (e error) {
+	var s string
+	if e = json.Unmarshal(b, &s); e != nil {
+		return e
+	}
+	if len(s) != 0 {
+		*i, e = ParseIMSI(s)
+	}
+	return e
 }
 
 // MCC value of this IMSI
 func (i IMSI) MCC() string {
-	if len(i.string) < 3 {
+	if len(i) < 3 {
 		return ""
 	}
-	return i.string[0:3]
+	return string(i[0:3])
 }
 
 // MNC2 return 2 charactor MNC value of this IMSI
 func (i IMSI) MNC2() string {
-	if len(i.string) < 5 {
+	if len(i) < 5 {
 		return ""
 	}
-	return i.string[3:5]
+	return string(i[3:5])
 }
 
 // MNC3 return 2 charactor MNC value of this IMSI
 func (i IMSI) MNC3() string {
-	if len(i.string) < 6 {
+	if len(i) < 6 {
 		return ""
 	}
-	return i.string[3:6]
+	return string(i[3:6])
 }
